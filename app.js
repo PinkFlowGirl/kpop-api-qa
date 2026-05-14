eedba5c938a99ade77cf4deeaf73884f1a0ebd81
Claudia Mangueira <claudiaoliveiramangueira@gmail.com>
2026-05-14 08:50:14 -0300

    feat: adiciona CORS na API KGA-14

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
