12e11fb9a07b740b19dc81e5c303320f4a6b6d47
Claudia Mangueira <claudiaoliveiramangueira@gmail.com>
2026-05-13 23:46:45 -0300

    fix: corrige URL do servidor Swagger para producao na Vercel

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -9,11 +9,13 @@ const options = {
       version: '1.0.0',
       description: 'API para gerenciamento de grupos de K-pop'
     },
-    servers: [
-      {
-        url: 'http://localhost:3000/api'
-      }
-    ],
+  servers: [
+  {
+    url: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}/api`
+      : 'http://localhost:3000/api'
+  }
+],
     components: {
       securitySchemes: {
         bearerAuth: {
